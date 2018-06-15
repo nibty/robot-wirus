31c765d49cf211649b1642395d1ef86eca2b8811
Nicholas Pettas <npettas@gmail.com>
2018-06-14 23:24:43 -0700

    check if bluetooth is connected already

--- scripts/pair_bt_speaker.sh
+++ scripts/pair_bt_speaker.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-bluetoothctl << EOF
-connect FC:58:FA:2F:59:55
+#connect FC:58:FA:2F:59:55
+info=$(bluetoothctl << EOF
+info FC:58:FA:2F:59:55
 EOF
+)
+
+is_connected=$(echo $info | egrep "Connected: yes" | wc -l)
+
+#echo $is_connected
+if [ "$is_connected" == 0 ]; then
+    echo "trying to connect.."
+bluetoothctl << EOF
+ connect FC:58:FA:2F:59:55
+EOF
+else
+    echo "already connected"
+fi
